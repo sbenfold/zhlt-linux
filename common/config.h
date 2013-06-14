ad80f258a5761f52e1bf6fb97e0ad7c0ab046aad
Simon Benfold <simon@benfold.com>
2013-06-14 21:33:18 +0100

    Added basic 32/64-bit detection to fix build.

--- common/config.h
+++ common/config.h
@@ -20,4 +20,28 @@
 //#include <unistd.h>
 #endif // SYSTEM_POSIX
 
+#if _WIN32 || _WIN64
+	#if _WIN64
+		#define ENV64BIT
+	#else
+		#define ENV32BIT
+	#endif
+#endif
+
+#if __GNUC__
+	#if __x86_64__ || __ppc64__
+		#define ENV64BIT
+	#else
+		#define ENV32BIT
+	#endif
+#endif
+
+#if defined(ENV32BIT)
+	#define SIZEOF_CHARP 4
+#elif defined(ENV64BIT)
+	#define SIZEOF_CHARP 8
+#else
+	#error "Failed to identify architecture"
+#endif
+
 #endif // CONFIG_H
